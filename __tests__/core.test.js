4b53f7906439bbc8e2990b6c54f6452e6c8226d7
zluuba <zluyba.nikitina@gmail.com>
2023-08-10 14:32:53 +0300

    fix: update tests

--- __tests__/core.test.js
+++ __tests__/core.test.js
@@ -13,11 +13,11 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readThisFile = (filename) => fsp.readFile(filename, 'utf-8');
 
 const resources = {
-  dir: 'ru-hexlet-io-courses_files',
+  dir: '/ru-hexlet-io-courses_files/',
   html: 'ru-hexlet-io-courses.html',
   png: 'ru-hexlet-io-assets-professions-nodejs.png',
   css: 'ru-hexlet-io-assets-application.css',
-  js: 'ru-hexlet-io-packs-js-runtime.js',
+  js: 'ru-hexlet-io-packs-js-runtime.js'
 };
 
 let tempDir;
@@ -28,40 +28,95 @@ let pngResource;
 let cssResource;
 let jsResource;
 
-nock.disableNetConnect();
+const fixtureDir = getFixturePath(resources.dir);
 
 beforeAll(async () => {
-  htmlAfter = await readThisFile(getFixturePath(resources.html));
+  nock.disableNetConnect();
 
-  const fixtureDir = getFixturePath(resources.dir);
+  htmlAfter = await readThisFile(getFixturePath(resources.html));
   htmlBefore = await readThisFile(path.join(fixtureDir, resources.html));
   pngResource = await readThisFile(path.join(fixtureDir, resources.png));
   cssResource = await readThisFile(path.join(fixtureDir, resources.css));
   jsResource = await readThisFile(path.join(fixtureDir, resources.js));
 });
 
+beforeEach(async () => {
+  tempDir = os.tmpdir();;
+});
+
 test('downloadPage main', async () => {
-  nock(/ru\.hexlet\.io/)
-    .get(/\/courses/)
+  nock('https://ru.hexlet.io')
+    .get('/courses')
     .reply(200, htmlBefore)
-    .get(/\/assets\/professions\/nodejs\.png/)
-    .reply(200, pngResource)
-    .get(/\/assets\/application\.css/)
+    .get('/assets/application.css')
     .reply(200, cssResource)
-    .get(/\/packs\/js\/runtime\.js/)
+    .get('/assets/professions/nodejs.png')
+    .reply(200, pngResource)
+    .get('/packs/js/runtime.js')
     .reply(200, jsResource);
 
-  tempDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  const loadedPageName = await downloadPage('https://ru.hexlet.io/courses', tempDir);
+  expect(loadedPageName).toEqual(resources.html);
 
-  await downloadPage('https://ru.hexlet.io/courses', tempDir);
-
-  // const expectedPng = await fsp.readFile(path.join(tempDir, resources.dir, resources.png), 'utf-8');
-  // const expectedCss = await fsp.readFile(path.join(tempDir, resources.dir, resources.css), 'utf-8');
-  // const expectedJs = await fsp.readFile(path.join(tempDir, resources.dir, resources.js), 'utf-8');
   const expectedHtml = await readThisFile(path.join(tempDir, resources.html));
-
-  // expect(expectedPng).toEqual(pngResource);
-  // expect(expectedCss).toEqual(cssResource);
-  // expect(expectedJs).toEqual(jsResource);
   expect(expectedHtml).toEqual(htmlAfter);
+
+  const expectedCss = await fsp.readFile(path.join(tempDir, resources.dir, resources.css), 'utf-8');
+  expect(expectedCss).toEqual(cssResource);
+
+  const expectedPng = await readThisFile(path.join(tempDir, resources.dir, resources.png));
+  expect(expectedPng).toEqual(pngResource);
+
+  const expectedJs = await fsp.readFile(path.join(tempDir, resources.dir, resources.js), 'utf-8');
+  expect(expectedJs).toEqual(jsResource);
+});
+
+test('Error: wrong outpath', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, htmlBefore)
+  
+  expect(downloadPage('https://ru.hexlet.io/courses', '/wrong/')).rejects.toThrow();
+});
+
+test('Error: page loading - 404 status code', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(404)
+  
+  expect(downloadPage('https://ru.hexlet.io/courses', tempDir)).rejects.toThrow();
+});
+
+test('Error: page loading - 500 status code', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(500)
+  
+  expect(downloadPage('https://ru.hexlet.io/courses', tempDir)).rejects.toThrow();
+});
+
+test('Error: resourses loading - 404 status code', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, htmlBefore)
+    .get('/assets/application.css')
+    .reply(404)
+    .get('/assets/professions/nodejs.png')
+    .reply(404)
+    .get('/packs/js/runtime.js')
+    .reply(404);
+
+  expect(downloadPage('https://ru.hexlet.io/courses', tempDir)).rejects.toThrow();
+});
+
+test('Error: page loading - network error', async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .replyWithError()
+  
+  expect(downloadPage('https://ru.hexlet.io/courses', tempDir)).rejects.toThrow();
+});
+
+afterEach(() => {
+  nock.cleanAll();
 });
